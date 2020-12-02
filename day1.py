242d513d49f611b30c26ee19bea4b9d37905a3fa
Jeff Tillotson <jpt@jeffx.com>
2020-12-01 21:44:14 -0500

    Put the different parts into functions
    - some PEP8 cleanup

--- day1.py
+++ day1.py
@@ -1,15 +1,24 @@
-import sys
+def part1(d):
+    for i in d:
+        for j in d:
+            if i + j == 2020:
+                print(i * j)
+                return
+
+
+def part2(d):
+    for i in d:
+        for j in d:
+            for x in d:
+                if i + j + x == 2020:
+                    print(i * j * x)
+                    return
 
 
 if __name__ == '__main__':
     data = []
-    with open("day1.dat", "r") as f:
-        for l in f:
-            data.append(int(l.rstrip()))
-    for i in data:
-        for j in data:
-            for x in data:
-                if i + j + x == 2020:
-                    print(i * j * x)
-                    sys.exit(0)
-
+    with open("day1.dat", "r") as data_file:
+        for line in data_file:
+            data.append(int(line.rstrip()))
+    part1(data)
+    part2(data)
