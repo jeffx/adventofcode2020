4f0fd1cb501c29a5e12bde31065a610015aea2b0
Jeff Tillotson <jpt@jeffx.com>
2020-12-05 00:33:57 -0500

    Day 5 - As submitted

--- day5.py
+++ day5.py
@@ -3,9 +3,8 @@ if __name__ == "__main__":
     with open("./day5.dat", "r") as infile:
         for line in infile:
             data.append(line.rstrip())
-    # data = ["FBFBBFFRLR"]
-    #for d in data[0:1]:
     max_seat = 0
+    seats = [None] * 900
     for d in data:
         r = [x for x in range(0, 128)]
         for x in range(0, 7):
@@ -13,17 +12,25 @@ if __name__ == "__main__":
                 r = r[:len(r) // 2]
             if d[x] == "B":
                 r = r[len(r) // 2:]
-        print(r[0])
         s = [x for x in range(0, 8)]
         for x in range(7, 10):
             if d[x] == "R":
                 s = s[len(s) // 2:]
             if d[x] == "L":
                 s = s[:len(s) // 2]
-        print(s[0])
-        if (r[0] * 8 + s[0]) > max_seat:
-            max_seat = r[0] * 8 + s[0]
-    print(max_seat)
+        seat = r[0] * 8 + s[0]
+        seats[seat] = "X"
+        if seat > max_seat:
+            max_seat = seat
+    print(max_seat)  # Max Seat is the answer to Part 1
+    """I didn't understand the question for part 2.  So I 
+       just printed out what sounded right, and hoped the 
+       output would be obvious.  It was.
+    """
+    seats = seats[:max_seat]
+    for x in range(0, len(seats)):
+        if seats[x] != "X":
+            print(x)
 
 
 
